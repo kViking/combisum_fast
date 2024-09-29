c32e5d4ff35f23489e4a01a37f30b639e45e0e6a
kviking <kviking503@gmail.com>
2024-09-29 09:15:17 -0700

    Refactor error handling and display in combisum.py

--- combisum.py
+++ combisum.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def combisum(target, numbers, try_cuda) -> list[list[float], dict]:
     # Create a dictionary to store flags and error messages
     flag = {
@@ -15,12 +16,12 @@ def combisum(target, numbers, try_cuda) -> list[list[float], dict]:
     if target == 0:
         flag["error"] = "Target must be nonzero"  # Check if target is zero
         return [[], flag]
-    
+
     # Convert numbers to integers if they have two decimal places
-    int_numbers = [int(100*x) if round(100*x) == 100*x else None for x in numbers]
+    int_numbers = [int(100 * x) if round(100 * x) == 100 * x else None for x in numbers]
     int_numbers = [x for x in int_numbers if x is not None]
     numbers = numbers if len(int_numbers) != len(numbers) else int_numbers
-    target = int(100*target) if type(numbers[0]) == int else target
+    target = int(100 * target) if isinstance(numbers[0], int) else target
 
     if torch.cuda.is_available() and not try_cuda:
         device = torch.device("cuda")  # Use CUDA if available
@@ -34,9 +35,8 @@ def combisum(target, numbers, try_cuda) -> list[list[float], dict]:
         tensor = torch.tensor(numbers).to(device)  # Convert numbers to tensor and move to device
         target = torch.tensor(target).to(device)  # Convert target to tensor and move to device
     except RuntimeError as e:
-        flag["error"] = e
+        flag["error"] = str(e)
         flag["error"] += "\n CUDA error loading tensors. Try running on CPU."
-
 
     combinations = []
     for i in range(2, len(numbers) + 1):
@@ -54,7 +54,7 @@ def combisum(target, numbers, try_cuda) -> list[list[float], dict]:
             combs = torch.combinations(tensor, r=i, with_replacement=False)  # Generate combinations of length i
             combinations.append(combs)  # Add combinations to the list
             flag['combo_length'] = i  # Update the current combination length
-        except Exception as e:
+        except Exception:
             flag["error"] = "Reached memory limit. Increase VRAM or decrease candidates to process longer combinations"  # Handle exception when reaching computational limit
             break
 
@@ -63,6 +63,9 @@ def combisum(target, numbers, try_cuda) -> list[list[float], dict]:
         indices = torch.where(torch.sum(combo, dim=1) == target)[0]  # Find indices where sum of each combination equals target
         valid_combinations.extend(combo[indices].tolist())  # Add valid combinations to the list
 
-    valid_combinations = [[x/100 for x in y] for y in valid_combinations] if type(numbers[0]) == int else [[round(x, 2) for x in y] for y in valid_combinations]  # Convert back to original format
+    if isinstance(numbers[0], int):
+        valid_combinations = [[x / 100 for x in y] for y in valid_combinations]
+    else:
+        valid_combinations = [[round(x, 2) for x in y] for y in valid_combinations]  # Convert back to original format
 
     return [valid_combinations, flag]  # Return valid combinations and flag dictionary
--- main.py
+++ main.py
@@ -4,31 +4,37 @@ from combisum import combisum
 from flet_core.control_event import ControlEvent
 from flet import (
     Row,
+    Card,
     Page,
     Text,
-    Card,
     Column,
+    SafeArea,
     Container,
-    TextField,
+    Checkbox,
     TextAlign,
-    TextButton,
+    TextField,
+    ThemeMode,
     IconButton,
-    TextThemeStyle,
+    ScrollMode,
+    TextButton,
+    ButtonStyle,
+    AlertDialog,
     ElevatedButton,
+    TextThemeStyle,
     MainAxisAlignment,
     CrossAxisAlignment,
 )
 
 splash_good = False
 try:
-    import pyi_splash as splash # type: ignore
+    import pyi_splash as splash  # type: ignore
 
     splash.update_text("Combisum GPU.0")
     splash_good = True
 except ImportError:
     pass
 
-# Create history item class
+
 class HistoryItem(TextButton):
     def __init__(self, target: str, array: str, results, on_click_function) -> None:
         super().__init__()
@@ -36,7 +42,7 @@ class HistoryItem(TextButton):
         self.array = array
         self.results = results
         self.on_click = on_click_function
-        self.text = f"{self.target} | {self.array}"    
+        self.text = f"{self.target} | {self.array}"
         self.tooltip = "Re-run this calculation"
 
 
@@ -44,106 +50,118 @@ def main(page: Page) -> None:
     if splash_good:
         splash.close()
 
-    # Set page properties
     page.title = "Combisum GPU.0"
     page.vertical_alignment = MainAxisAlignment.START
     page.horizontal_alignment = CrossAxisAlignment.CENTER
 
+    input_title = Text(
+        "Input", theme_style=TextThemeStyle.DISPLAY_SMALL, text_align=TextAlign.LEFT
+    )
+    results_title = Text(
+        "Results", theme_style=TextThemeStyle.DISPLAY_SMALL, text_align=TextAlign.LEFT
+    )
+    history_title = Text(
+        "History", theme_style=TextThemeStyle.DISPLAY_SMALL, text_align=TextAlign.LEFT
+    )
 
-    # Set up variables
     results = []
     history = []
 
-    # Error display
     error = Text("")
     error_button = IconButton(
-        icon=ft.icons.ERROR, 
-        tooltip="Display error info", 
+        icon=ft.icons.ERROR,
+        tooltip="Display error info",
         on_click=lambda e: page.open(error_modal),
-        style=ft.ButtonStyle(color=ft.colors.RED),
-        visible=False
+        style=ButtonStyle(color=ft.colors.RED),
+        visible=False,
     )
 
     def close_error_modal(event: ControlEvent) -> None:
         page.close(error_modal)
 
-    error_modal = ft.AlertDialog(
+    error_modal = AlertDialog(
         title=Text("Error"),
-        content=Column([
-            error
-        ]),
-        actions=[TextButton("Close", on_click=close_error_modal)]
+        content=Column([error]),
+        actions=[TextButton("Close", on_click=close_error_modal)],
     )
 
-
-
-    # Create result_text and info_modal variables to update later
-    result_text = Column(expand=True, horizontal_alignment=CrossAxisAlignment.CENTER, alignment=MainAxisAlignment.CENTER)
+    result_text = Column(
+        expand=True,
+        horizontal_alignment=CrossAxisAlignment.CENTER,
+        alignment=MainAxisAlignment.CENTER,
+    )
 
     def close_info_modal(event: ControlEvent) -> None:
         page.close(info_modal)
 
-    info_modal = ft.AlertDialog(
+    info_modal = AlertDialog(
         title=Text("Info"),
         content=Column([]),
-        actions=[TextButton("Close", on_click=close_info_modal)]
+        actions=[TextButton("Close", on_click=close_info_modal)],
     )
 
-
-    # Define buttons and button callbacks
+    debug_button = IconButton(
+        icon=ft.icons.CODE,
+        on_click=lambda e: page.open(info_modal),
+        tooltip="Get debug info",
+    )
 
     def toggle_dark_mode(event: ControlEvent) -> None:
-        # Toggle dark mode
-        page.theme_mode = ft.ThemeMode.DARK if page.theme_mode == ft.ThemeMode.LIGHT else ft.ThemeMode.LIGHT
+        page.theme_mode = (
+            ThemeMode.DARK
+            if page.theme_mode == ThemeMode.LIGHT
+            else ThemeMode.LIGHT
+        )
         page.update()
 
-    dark_mode_button = ft.IconButton(icon=ft.icons.LIGHT_MODE, tooltip="Toggle dark mode", on_click=toggle_dark_mode)
+    dark_mode_button = IconButton(
+        icon=ft.icons.LIGHT_MODE, tooltip="Toggle dark mode", on_click=toggle_dark_mode
+    )
 
     def copy_results(event: ControlEvent) -> None:
-        # Copy results to clipboard as .tsv raw values
         tsv = "\n".join(["\t".join([str(y) for y in x]) for x in results[0]])
         pyperclip.copy(tsv)
 
     copy_button = IconButton(
-        icon=ft.icons.CONTENT_COPY, 
+        icon=ft.icons.CONTENT_COPY,
         on_click=copy_results,
         tooltip="Copy all results to clipboard as .tsv raw values",
-        visible=False)
+        visible=False,
+    )
 
     def load_history(event: ControlEvent) -> None:
         nonlocal target_text, array_text, results, history, error
-        
-        # Load selected history item into input fields
+
         target_text.value = event.control.text.split(" | ")[0]
         array_text.value = event.control.text.split(" | ")[1]
         results = []
         run(event)
         page.update()
-    
-    cpu_mode = ft.Checkbox("CPU only", value=False, tooltip="Restrict to CPU computation")
-    
+
+    cpu_mode = Checkbox("CPU only", value=False, tooltip="Restrict to CPU computation")
+
     def run(event: ControlEvent) -> None:
         nonlocal result_text, results, history, error
 
-        # Get target value and array from input fields
         target = float(target_text.value)
         array = array_text.value.replace(",", "")
         array = [float(x) for x in array.split()]
-        result_text.controls = [Text('beep boop', color=ft.colors.GREEN)]
+        result_text.controls = [Text("beep boop", color=ft.colors.GREEN)]
         page.update()
 
-        # Calculate combisum and format results
         results = combisum(target, array, cpu_mode.value)
-        result_formatted = [[', '.join(str(y) for y in x)] for x in results[0][:5]]
+        result_formatted = [[", ".join(str(y) for y in x)] for x in results[0][:5]]
 
         result_text.controls = [
-                TextButton(text=x[0],
-                    on_click=lambda e: pyperclip.copy("\t".join(x[0].split(", "))),
-                    tooltip="Copy this combination to clipboard",
-                    style=ft.ButtonStyle(color=ft.colors.ON_PRIMARY_CONTAINER)
-                ) for x in result_formatted
-            ] + [Text(f"...and {len(results[0]) - 5} more") if len(results[0]) > 5 else Text("")]
-        
+            TextButton(
+                text=x[0],
+                on_click=lambda e: pyperclip.copy("\t".join(x[0].split(", "))),
+                tooltip="Copy this combination to clipboard",
+                style=ButtonStyle(color=ft.colors.ON_PRIMARY_CONTAINER),
+            )
+            for x in result_formatted
+        ] + [Text(f"...and {len(results[0]) - 5} more") if len(results[0]) > 5 else Text("")]
+
         error.value = results[1].get("error", "")
         error_button.visible = bool(error.value)
         copy_button.visible = bool(results[0])
@@ -152,40 +170,42 @@ def main(page: Page) -> None:
 
         history.append(
             {
-            "target": target_text.value,
-            "array": array_text.value,
-            "results": results
+                "target": target_text.value,
+                "array": array_text.value,
+                "results": results,
             }
         )
-        
+
         history_card.visible = True
         history_view.controls = [
-            HistoryItem(target=x.get('target'), 
-                        array=x.get('array'), 
-                        results=x.get("results"), 
-                        on_click_function=load_history
-            ) for x in list(reversed(history))]
+            HistoryItem(
+                target=x.get("target"),
+                array=x.get("array"),
+                results=x.get("results"),
+                on_click_function=load_history,
+            )
+            for x in list(reversed(history))
+        ]
 
         for item in history_view.controls:
             if not item.results[0]:
-                item.style = ft.ButtonStyle(color=ft.colors.RED)
+                item.style = ButtonStyle(color=ft.colors.RED)
             else:
-                item.style = ft.ButtonStyle(color=ft.colors.ON_PRIMARY_CONTAINER)
+                item.style = ButtonStyle(color=ft.colors.ON_PRIMARY_CONTAINER)
 
         page.update()
 
-    # Create input fields and buttons
     target_text = TextField(value="25.43", on_submit=run)
     array_text = TextField(value="12.85 3.14 5.43 4.56 12.58", on_submit=run)
     button = ElevatedButton("Run", on_click=run)
     history_view = Column(
         expand=True,
-        scroll=ft.ScrollMode.ALWAYS,
+        scroll=ScrollMode.ALWAYS,
         horizontal_alignment=CrossAxisAlignment.START,
         alignment=MainAxisAlignment.START,
-        controls=[]
+        controls=[],
     )
-    
+
     input_card = Card(
         content=Container(
             expand=True,
@@ -194,22 +214,16 @@ def main(page: Page) -> None:
                 expand=True,
                 alignment=MainAxisAlignment.START,
                 controls=[
-                    Text('Input', 
-                        expand=True, 
-                        theme_style=TextThemeStyle.DISPLAY_SMALL, 
-                        text_align=TextAlign.LEFT),
+                    input_title,
                     target_text,
                     array_text,
                     Row(
                         expand=True,
                         alignment=MainAxisAlignment.END,
-                        controls=[
-                            cpu_mode,
-                            button
-                        ]
-                    )
-                ]
-            )
+                        controls=[cpu_mode, button],
+                    ),
+                ],
+            ),
         )
     )
 
@@ -222,15 +236,9 @@ def main(page: Page) -> None:
             content=Column(
                 expand=True,
                 alignment=MainAxisAlignment.START,
-                controls=[
-                    Text('History', 
-                        theme_style=TextThemeStyle.DISPLAY_SMALL, 
-                        text_align=TextAlign.LEFT, 
-                        expand=True),
-                    history_view
-                ]
-            )
-        )
+                controls=[history_title, history_view],
+            ),
+        ),
     )
 
     results_card = Card(
@@ -242,49 +250,32 @@ def main(page: Page) -> None:
                 controls=[
                     Row(
                         expand=True,
-                        controls=[
-                            error_button,
-                            Text('Results', 
-                            theme_style=TextThemeStyle.DISPLAY_SMALL, 
-                            text_align=TextAlign.LEFT,
-                            expand=True),
-                            copy_button
-                        ]
+                        controls=[error_button, results_title, copy_button],
                     ),
                     Row(
                         expand=True,
                         alignment=MainAxisAlignment.START,
                         vertical_alignment=CrossAxisAlignment.START,
-                        controls=[
-                            result_text,
-                        ]
+                        controls=[result_text],
                     ),
                     Row(
                         expand=True,
                         alignment=MainAxisAlignment.END,
-                        controls=[
-                            IconButton(
-                                icon=ft.icons.CODE, 
-                                on_click=lambda e: page.open(info_modal),
-                                tooltip="Get debug info"
-                            )
-                        ]
-                    )
-                ]
-            )
+                        controls=[debug_button],
+                    ),
+                ],
+            ),
         )
-    )           
+    )
 
-    page_layout = ft.SafeArea(
+    page_layout = SafeArea(
         Column(
             alignment=MainAxisAlignment.SPACE_AROUND,
             controls=[
                 Row(
                     expand=True,
                     alignment=MainAxisAlignment.END,
-                    controls=[
-                        dark_mode_button
-                    ]
+                    controls=[dark_mode_button],
                 ),
                 Row(
                     vertical_alignment=CrossAxisAlignment.START,
@@ -298,22 +289,23 @@ def main(page: Page) -> None:
                                     expand=True,
                                     alignment=MainAxisAlignment.START,
                                     vertical_alignment=CrossAxisAlignment.START,
-                                    controls=[history_card]
-                                )
-                            ]
+                                    controls=[history_card],
+                                ),
+                            ],
                         ),
                         Column(
                             expand=True,
                             alignment=MainAxisAlignment.START,
-                            controls=[results_card]
-                        )                        
-                    ] 
-                )
-            ]
+                            controls=[results_card],
+                        ),
+                    ],
+                ),
+            ],
         )
     )
 
     page.add(page_layout)
+
 
 if __name__ == "__main__":
     ft.app(main)
